6aa048d000e83e7e157742b83854e76bd6593570
Soucius <soucius.ggwp@gmail.com>
2024-08-12 18:58:12 +0300

    next.js css styles and layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import Navbar from '@/components/navbar/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +16,11 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <div className="container">
           <div className="wrapper">
+            <Navbar />
+
             {children}
+
+            <Footer />
           </div>
         </div>
       </body>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,7 @@
-import Link from "next/link";
-import styles from "./homepage.module.css";
-
 export default function Home() {
-  return <div>
-    <Link href="/">
-      <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus accusamus dolores non harum molestias alias ex accusantium exercitationem unde odit voluptatum neque, aperiam modi voluptates eligendi odio maiores dolor et.</h1>
-
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos, saepe quaerat! Minus accusantium obcaecati rerum, nesciunt, fuga suscipit ullam corporis quo architecto totam qui hic magnam beatae, quae quas sunt repudiandae saepe deserunt dolorum autem. Ab quis nisi eligendi laboriosam dolorum totam et repellat maxime, temporibus ad fugit fugiat accusamus laudantium consequuntur! Laboriosam quo praesentium velit ea dicta ullam incidunt voluptatibus cumque cum rerum qui ad itaque aliquam a dolore, soluta sed placeat vel. Voluptatibus, rerum repellendus libero eveniet vero dignissimos itaque ipsum veniam accusamus, a similique exercitationem reprehenderit molestias earum corrupti vel! Ullam praesentium soluta nemo aut fugiat veritatis.
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos, saepe quaerat! Minus accusantium obcaecati rerum, nesciunt, fuga suscipit ullam corporis quo architecto totam qui hic magnam beatae, quae quas sunt repudiandae saepe deserunt dolorum autem. Ab quis nisi eligendi laboriosam dolorum totam et repellat maxime, temporibus ad fugit fugiat accusamus laudantium consequuntur! Laboriosam quo praesentium velit ea dicta ullam incidunt voluptatibus cumque cum rerum qui ad itaque aliquam a dolore, soluta sed placeat vel. Voluptatibus, rerum repellendus libero eveniet vero dignissimos itaque ipsum veniam accusamus, a similique exercitationem reprehenderit molestias earum corrupti vel! Ullam praesentium soluta nemo aut fugiat veritatis.
-    </Link>
-  </div>;
+  return (
+    <div>
+      
+    </div>
+  );
 }
